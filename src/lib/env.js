835d08437cb56144e5233438d3f1af3036f9773d
version-1 <12.05.xxxx@gmail.com>
2019-01-10 19:11:19 +0900

    new post

--- src/lib/env.js
+++ src/lib/env.js
@@ -2,8 +2,8 @@ import { meta } from '../../config/constants'
 export const isProductionHost = function() {
   // FIXME 一時的に全てtrue
   return true
-  const hostname = typeof window !== 'undefined' && window.location.host
-  return meta.hostname === hostname
+  // const hostname = typeof window !== 'undefined' && window.location.host
+  // return meta.hostname === hostname
 }()
 export const isEnv = (env) => process.env.NODE_ENV === env
 export const isProduction = isEnv('production')
