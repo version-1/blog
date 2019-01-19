eae163062ba92f049377cf80b42f290780bcbc5f
version-1 <12.05.xxxx@gmail.com>
2019-01-19 11:46:17 +0900

    some fix

--- src/components/organisms/ArchiveByMonth.js
+++ src/components/organisms/ArchiveByMonth.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import moment from 'moment';
 
+const ARCHIVE_LIMIT = 12;
 const ArchiveByMonth = ({items}) => {
   if (!items) return <div></div>;
+
+  const sortedKeys = Object.keys(items).sort((a, b) => {
+    if (moment(a, 'YYYY/MM').isAfter(moment(b, 'YYYY/MM'))) {
+       return -1
+    } else {
+       return 1
+    }
+  })
   return (
     <div className="archive-by-month">
       <h3>月別アーカイブ</h3>
       <ul className="archive-by-month-body">
-        {Object.keys(items).map(key => {
+        {sortedKeys.slice(0, ARCHIVE_LIMIT).map(key => {
           if (!items[key]) return;
           return (
-            <li>
+            <li key={key}>
               <a href={`/${key}`}>
                 {key} ({items[key].length})
               </a>
