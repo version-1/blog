4171f02981a1a138fa929fbfce2a1c1de9269582
version-1 <12.05.xxxx@gmail.com>
2019-06-04 01:07:23 +0900

    fix link

--- src/components/molecules/CategoryList.js
+++ src/components/molecules/CategoryList.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import i18next from 'lib/i18next';
 import {categoryPath} from 'lib/routes';
-const CategoryList = ({list, delimiter}) => {
+const CategoryList = ({language, list, delimiter}) => {
   return list.map((category, index) => {
     const name = i18next.t(`categories.${category}`);
     return (
-      <a key={category} href={categoryPath(category)} className="category btn-flat">
+      <a key={category} href={categoryPath(category, language)} className="category btn-flat">
         {name}
       </a>
     );
--- src/components/molecules/TagList.js
+++ src/components/molecules/TagList.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 import i18next from 'lib/i18next';
 import {tagPath} from 'lib/routes';
-const TagList = ({list, delimiter}) => {
+const TagList = ({language, list, delimiter}) => {
   return list.map((tag, index) => {
     const name = i18next.t(`tags.${tag}`);
     return (
-      <a key={tag} href={tagPath(tag)} className="tag btn-flat">
+      <a key={tag} href={tagPath(tag, language)} className="tag btn-flat">
         {name}
       </a>
     );
   });
 };
 
-export default TagList
+export default TagList;
