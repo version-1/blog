c821b5df8221c34c4a78e872c1f08d7223f84186
version-1 <12.05.xxxx@gmail.com>
2019-06-11 21:53:33 +0900

    fix related-posts

--- src/components/organisms/RelatedPost.js
+++ src/components/organisms/RelatedPost.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {meta} from 'config/constants';
 import {Link} from 'gatsby';
+import i18next from 'lib/i18next';
 
 const RelatedPost = edges => {
-  if (!edges) {
+  if (!edges || !edges.related) {
     return <div />;
   }
   const {edges: posts} = edges.related;
   return (
     <div className="related-posts">
-      <h2 className="related-post-title">関連記事</h2>
+      <h2 className="related-post-title">{i18next.t('labels.related-posts')}</h2>
       <div className="row related-post-content">
         {posts.map(post => {
           const {slug, title, thumbnail} = post.node.frontmatter;
