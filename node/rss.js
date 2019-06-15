bdbdd184cb8b7f46a11145c3beccb48a46193abb
version-1 <12.05.xxxx@gmail.com>
2019-06-15 16:24:04 +0900

    fix rss

--- node/rss.js
+++ node/rss.js
@@ -1,9 +1,16 @@
-
-const serialize = (params) => {
-  const {query: {site, allMarkdownRemark}} = params
+const serialize = params => {
+  const {
+    query: {site, allMarkdownRemark},
+  } = params;
   return allMarkdownRemark.edges.map(edge => {
+    const {language} = edge.node.frontmatter;
     const url =
-      site.siteMetadata.siteUrl + edge.node.frontmatter.slug;
+      language === 'ja'
+        ? site.siteMetadata.siteUrl + edge.node.frontmatter.slug
+        : [
+            site.siteMetadata.siteUrl,
+            '/' + language + edge.node.frontmatter.slug,
+          ].join('');
     const result = Object.assign({}, edge.node.frontmatter, {
       description: edge.node.excerpt,
       date: edge.node.frontmatter.createdAt,
@@ -11,12 +18,12 @@ const serialize = (params) => {
       guid: url,
       custom_elements: [{'content:encoded': edge.node.html}],
     });
-    return result
+    return result;
   });
-}
+};
 
 const queries = language => {
-  const array = '["' + language.join('","') + '" ]'
+  const array = '["' + language.join('","') + '" ]';
   return `{
     site {
       siteMetadata {
@@ -43,18 +50,17 @@ const queries = language => {
           fields { slug }
           frontmatter {
             title
+            language
             slug
             createdAt(formatString: "MMM DD, YYYY")
           }
         }
       }
     }
-  }`
-}
-
+  }`;
+};
 
 module.exports = {
   serialize,
-  queries
-}
-
+  queries,
+};
