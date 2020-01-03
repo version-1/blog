622c7f650c70b28bf44b303109d1c5c19bb35665
version-1 <12.05.xxxx@gmail.com>
2020-01-02 17:04:39 -0800

    brek style&remove materialize

--- src/components/layouts/Index.js
+++ src/components/layouts/Index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import {StaticQuery, graphql} from 'gatsby';
 import slack from 'slack-notify';
 import {batchEventRegister} from 'ga-event-tracker-on-site';
-import headerImage from 'assets/images/header-image-2.svg';
-import headerImageSmall from 'assets/images/header-image-small.svg';
-import footerImage from 'assets/images/footer-image.svg';
 
-import Img from 'components/atoms/Image';
 import Navbar from 'components/Navbar';
 import Head from 'components/Head';
 import 'assets/stylesheets/index.sass';
@@ -52,42 +48,12 @@ export default class IndexLayout extends React.PureComponent {
               siteTitle={data.site.siteMetadata.title}
               description={data.site.siteMetadata.description}
             />
-            <div className="header-image hide-on-med-and-down">
-              <Img
-                amp={amp}
-                width={4000}
-                height={300}
-                layout="fixed"
-                src={headerImage}
-                alt="header image"
-              />
-            </div>
-            <div className="header-image-small hide-on-large-only">
-              <Img
-                amp={amp}
-                width={2000}
-                height={150}
-                layout="fixed"
-                src={headerImageSmall}
-                alt="header image"
-              />
-            </div>
             <Navbar language={language} amp={amp} />
             <div>{children}</div>
             <div className="footer">
               <span className="copyright">
                 Copyright © 2018 So Far , So Tech All Rights Reserved.
               </span>
-            </div>
-            <div className="footer-image">
-              <Img
-                amp={amp}
-                width={4000}
-                height={300}
-                layout="fixed"
-                src={footerImage}
-                alt="footer image"
-              />
             </div>
           </div>
         )}
