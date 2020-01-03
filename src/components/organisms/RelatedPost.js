ad40551aa99ef933d04e609f4e808ebfffd41704
version-1 <12.05.xxxx@gmail.com>
2020-01-02 19:37:09 -0800

    fix related-post

--- src/components/organisms/RelatedPost.js
+++ src/components/organisms/RelatedPost.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {meta} from 'config/constants';
-import {Link} from 'gatsby';
 import i18next from 'lib/i18next';
+import PostList from 'components/organisms/PostList';
 
 const RelatedPost = edges => {
   if (!edges || !edges.related) {
@@ -10,27 +9,10 @@ const RelatedPost = edges => {
   const {edges: posts} = edges.related;
   return (
     <div className="related-posts">
-      <h2 className="related-post-title">{i18next.t('labels.related-posts')}</h2>
-      <div className="row related-post-content">
-        {posts.map(post => {
-          const {slug, title, thumbnail} = post.node.frontmatter;
-          const url = meta.images.url + thumbnail;
-          return (
-            <div key={slug} className="col s12 m4">
-              <Link to={slug}>
-                <div className="card related-post">
-                  <div className="card-image related-img">
-                    <img src={url} alt={title} />
-                  </div>
-                  <div className="card-content related-content">
-                    <h3>{title}</h3>
-                  </div>
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
+      <h2 className="related-post-title">
+        {i18next.t('labels.related-posts')}
+      </h2>
+      <PostList posts={posts} />
     </div>
   );
 };
--- src/templates/404.js
+++ src/templates/404.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Layout from 'components/layouts/Default.js';
-import Post from 'components/Post';
+import PostList from 'components/organisms/PostList';
 
-const NotFoundPage = (props) => {
+const NotFoundPage = props => {
   const context = props.pageContext;
   const {edges: posts} = props.pageContext.popPosts.data.allMarkdownRemark;
   return (
@@ -11,22 +11,7 @@ const NotFoundPage = (props) => {
         <h1>404 NOT FOUND</h1>
         <p>お探しのページが見つかりません。</p>
       </div>
-      <section className="section">
-        <div className="section-container">
-          <div className="section-content">
-            <div className="section-title">
-              <span className="title">人気記事</span>
-            </div>
-            <div className="section-list">
-              <div className="row">
-                {posts.map(({node: post}) => (
-                  <Post post={post} key={post.id} />
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <PostList titleLabel="labels.pop-posts" posts={posts} />
     </Layout>
   );
 };
