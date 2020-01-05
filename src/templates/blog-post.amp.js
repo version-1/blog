cbb0380253dad67790c8678027b1f6d8470ce2ee
version-1 <12.05.xxxx@gmail.com>
2020-01-05 13:21:59 -0800

    some fix

--- src/templates/blog-post.amp.js
+++ src/templates/blog-post.amp.js
@@ -2,11 +2,18 @@ import BlogPost from 'templates/blog-post';
 import {graphql} from 'gatsby';
 
 export const pageQuery = graphql`
-  query BlogPostAMPByID($id: String!, $related: [String]) {
+  query BlogPostAMPByID($id: String!, $related: [String], $pickup: [String]) {
     markdownRemark(id: {eq: $id}) {
       id
       html
       excerpt(truncate: true, pruneLength: 300)
+      thumbnail {
+        childImageSharp {
+          fluid(maxWidth: 796) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
       frontmatter {
         language
         title
@@ -17,27 +24,59 @@ export const pageQuery = graphql`
         updatedAt(formatString: "MMM DD, YYYY")
       }
     }
-    allMarkdownRemark(
+    pickup: allMarkdownRemark(filter: {frontmatter: {slug: {in: $pickup}}}) {
+      totalCount
+      nodes {
+        id
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          language
+          slug
+          thumbnail
+          templateKey
+          categories
+          tags
+          createdAt(formatString: "MMM DD, YYYY")
+          updatedAt(formatString: "MMM DD, YYYY")
+        }
+        thumbnail {
+          childImageSharp {
+            fluid(maxWidth: 796) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+    related: allMarkdownRemark(
       sort: {order: DESC, fields: [frontmatter___createdAt]}
       filter: {frontmatter: {slug: {in: $related}}}
     ) {
       totalCount
-      edges {
-        node {
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            slug
-            language
-            thumbnail
-            templateKey
-            categories
-            tags
-            createdAt(formatString: "MMM DD, YYYY")
-            updatedAt(formatString: "MMM DD, YYYY")
+      nodes {
+        id
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          language
+          slug
+          thumbnail
+          templateKey
+          categories
+          tags
+          createdAt(formatString: "MMM DD, YYYY")
+          updatedAt(formatString: "MMM DD, YYYY")
+        }
+        thumbnail {
+          childImageSharp {
+            fluid(maxWidth: 796) {
+              ...GatsbyImageSharpFluid
+            }
           }
         }
       }
