da28752b612fc526c7f99010e18248d18adf5e20
version-1 <12.05.xxxx@gmail.com>
2020-01-05 19:08:09 -0800

    fix 404 page

--- src/templates/404.js
+++ src/templates/404.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import Layout from 'components/layouts/Default.js';
+import {graphql} from 'gatsby';
 import PostList from 'components/organisms/PostList';
 
 const NotFoundPage = props => {
-  const context = props.pageContext;
-  const posts = context.pickup ? context.pickup.data.allMarkdownRemark.edges : [];
+  const {pickup, language, amp, baseUrl, layout} = props.pageContext;
+  const pickupList = pickup ? props.data.pickup.nodes : [];
   return (
-    <Layout {...context}>
+    <Layout
+      amp={amp}
+      baseUrl={baseUrl}
+      pickup={pickupList}
+      language={language}
+      layout={layout}>
       <div className="not-found">
         <h1>404 NOT FOUND</h1>
         <p>お探しのページが見つかりません。</p>
       </div>
-      <PostList titleLabel="labels.pickup" posts={posts} />
+      <PostList titleLabel="labels.pickup" posts={pickupList} />
     </Layout>
   );
 };
 
 export default NotFoundPage;
+
+export const pageQuery = graphql`
+  query NotFoundPageQuery($language: String!, $pickup: [String]) {
+    pickup: allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          templateKey: {eq: "blog-post"}
+          language: {eq: $language}
+          slug: {in: $pickup}
+        }
+      }
+    ) {
+      totalCount
+      nodes {
+        id
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          language
+          slug
+          thumbnail
+          templateKey
+          categories
+          tags
+          createdAt(formatString: "MMM DD, YYYY")
+          updatedAt(formatString: "MMM DD, YYYY")
+        }
+        thumbnail {
+          childImageSharp {
+            fluid(maxWidth: 796) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
