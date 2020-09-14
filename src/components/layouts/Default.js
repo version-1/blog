61eb945eb0cbf8fd9d2eda20824c4b2a78f456e0
version-1 <12.05.xxxx@gmail.com>
2020-09-14 16:13:44 -0700

    some fix

--- src/components/layouts/Default.js
+++ src/components/layouts/Default.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import {Link} from 'gatsby';
-import Layout from 'components/layouts/Index';
-import Sidebar from 'components/Sidebar';
-import PickupList from 'components/organisms/PickupList';
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "components/layouts/Index";
+import Sidebar from "components/Sidebar";
+import PickupList from "components/organisms/PickupList";
+import { useDeviceType } from "../../hooks/useDeviceType";
 
-const Breadcrumbs = ({context}) => {
+const Breadcrumbs = ({ context }) => {
   return (
     <ul className="breadcrumbs">
       {context.map((item, idx) => {
@@ -18,37 +19,39 @@ const Breadcrumbs = ({context}) => {
   );
 };
 
-class DefaultLayout extends React.PureComponent {
-  render() {
-    const {pickupDisabled, pickup, language, baseUrl, layout} = this.props;
-    const {archiveByMonth, breadcrumbs = []} = layout;
-    return (
-      <Layout language={language} baseUrl={baseUrl}>
-        <main className="row container">
-          <Breadcrumbs context={breadcrumbs} />
-          {!pickupDisabled && <PickupList posts={pickup} />}
-          <section className="flex">
-            <div className="main">{this.props.children}</div>
-            <div className="hide-on-med-and-down">
-              <Sidebar
-                layout={layout}
-                language={language}
-                archiveByMonth={archiveByMonth}
-              />
-            </div>
-          </section>
-          <div className="flex">
-            <div className="hide-on-large-only">
-              <Sidebar
-                layout={layout}
-                language={language}
-              />
-            </div>
+const DefaultLayout = ({
+  children,
+  pickupDisabled,
+  pickup,
+  language,
+  baseUrl,
+  layout
+}) => {
+  const { smartphone } = useDeviceType();
+  const { archiveByMonth, breadcrumbs = [] } = layout;
+  return (
+    <Layout language={language} baseUrl={baseUrl}>
+      <main className="row container">
+        <Breadcrumbs context={breadcrumbs} />
+        {!pickupDisabled && !smartphone && <PickupList posts={pickup} />}
+        <section className="flex">
+          <div className="main">{children}</div>
+          <div className="hide-on-med-and-down">
+            <Sidebar
+              layout={layout}
+              language={language}
+              archiveByMonth={archiveByMonth}
+            />
           </div>
-        </main>
-      </Layout>
-    );
-  }
-}
+        </section>
+        <div className="flex">
+          <div className="hide-on-large-only">
+            <Sidebar layout={layout} language={language} />
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+};
 
-export default DefaultLayout;
+export default React.memo(DefaultLayout, []);
