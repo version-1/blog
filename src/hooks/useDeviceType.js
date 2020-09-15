fade124c25ecffcd606949da05485d02b2e511c4
version-1 <12.05.xxxx@gmail.com>
2020-09-14 17:55:01 -0700

    some fix

--- src/hooks/useDeviceType.js
+++ src/hooks/useDeviceType.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
 
 export const useDeviceType = () => {
+  const [ready, setReady] = useState(false);
   const [smartphone, setSmartPhone] = useState(false);
   useEffect(() => {
     if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
       setSmartPhone(true)
     }
+    setReady(true)
   }, []);
 
   return {
+    ready,
     smartphone
   }
 };
