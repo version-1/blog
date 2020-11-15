ae865bca9516e2d1098e8e287746128aff8303e5
version-1 <12.05.xxxx@gmail.com>
2020-11-14 16:14:12 -0800

    add prettierrc

--- src/components/atoms/Link.js
+++ src/components/atoms/Link.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const LinkComponent = ({ children, ...rest }) => {
-  const path =
-    typeof window === 'undefined' ? undefined : window.location.pathname
+  const path = typeof window === 'undefined' ? undefined : window.location.pathname
   if (!rest.to && path === rest.to) {
     return <a href={path}>{children}</a>
   }
