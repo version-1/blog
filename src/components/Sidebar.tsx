bb5d259910298aa7d243ae7e6f8d96f5699634ad
version-1 <j.onodera26@gmail.com>
2021-03-13 22:23:26 -0800

    apply gatsby-plugin-image

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,6 @@
-import React, { PureComponent } from 'react'
-import { useStaticQuery, graphql, Link } from 'gatsby'
-import Img from 'gatsby-image'
-import profile from 'assets/images/profile-small.png'
+import React from 'react'
+import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import ArchiveByMonth from 'components/organisms/ArchiveByMonth'
 import { tagPath, categoryPath, aboutPath } from 'lib/routes'
 import { instance as i18next } from 'lib/i18next'
@@ -9,19 +8,15 @@ import TagCloud from 'components/molecules/TagCloud'
 import Title from 'components/molecules/Title'
 
 const Profile = () => {
-  const data = useStaticQuery(graphql`
-    {
-      profile: file(relativePath: { eq: "profile.png" }) {
-        childImageSharp {
-          fixed(width: 100, height: 100) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `)
-
-  return <Img fixed={data.profile.childImageSharp.fixed} alt="profile" />
+  return (
+    <StaticImage
+      src="../assets/images/profile.png"
+      alt="profile"
+      width={64}
+      height={64}
+      layout="fixed"
+    />
+  )
 }
 
 const displayCategories = (language: Lang) => [
@@ -31,71 +26,60 @@ const displayCategories = (language: Lang) => [
   tagPath('gadget', language),
   tagPath('english', language),
   categoryPath('design', language),
-  categoryPath('column', language),
+  categoryPath('column', language)
 ]
 
-export default class Sidebar extends PureComponent {
-  render() {
-    const {
-      language,
-      layout: { archiveByMonth, tags },
-    } = this.props
+const Sidebar = (props: any) => {
+  const {
+    language,
+    layout: { archiveByMonth, tags }
+  } = props
 
-    console.log(i18next)
-    console.log(i18next.t('labels.article'))
-    return (
-      <div className="sidebar">
-        <section className="section">
-          <Title color="skyblue" label="labels.sidebar.profile-title" />
-          <div className="self-introduction">
-            <div className="profile-image">
-              <Profile
-                width="100"
-                height="100"
-                layout="fixed"
-                src={profile}
-                alt="profile"
-              />
-            </div>
-            <div className="introduction">
-              {i18next.t('labels.sidebar.profile-description')}
-              <p>
-                <Link to={aboutPath(language)}>
-                  {i18next.t('labels.sidebar.profile-link')}
-                </Link>
-              </p>
-            </div>
+  return (
+    <div className="sidebar">
+      <section className="section">
+        <Title color="skyblue" label="labels.sidebar.profile-title" />
+        <div className="self-introduction">
+          <div className="profile-image">
+            <Profile />
           </div>
-        </section>
-        <section className="section">
-          <Title color="skyblue" label="labels.sidebar.categories" />
-          <div className="sidebar-categories">
-            <ul className="sidebar-categories-list">
-              {displayCategories(language).map(category => {
-                const key = category.replace(/\//g, '.').slice(1)
-                const _key = language === 'ja' ? key : key.replace('en.', '')
-                return (
-                  <li key={_key} className="sidebar-categories-item">
-                    <Link
-                      className="sidebar-categories-item-link"
-                      to={category}
-                    >
-                      {i18next.t(_key)}
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
+          <div className="introduction">
+            {i18next.t('labels.sidebar.profile-description')}
+            <p>
+              <Link to={aboutPath(language)}>
+                {i18next.t('labels.sidebar.profile-link')}
+              </Link>
+            </p>
           </div>
-        </section>
-        <section className="section">
-          <Title color="skyblue" label="labels.sidebar.tags" />
-          <TagCloud language={language} tags={tags} />
-        </section>
-        <section className="section">
-          <ArchiveByMonth language={language} items={archiveByMonth} />
-        </section>
-      </div>
-    )
-  }
+        </div>
+      </section>
+      <section className="section">
+        <Title color="skyblue" label="labels.sidebar.categories" />
+        <div className="sidebar-categories">
+          <ul className="sidebar-categories-list">
+            {displayCategories(language).map((category) => {
+              const key = category.replace(/\//g, '.').slice(1)
+              const _key = language === 'ja' ? key : key.replace('en.', '')
+              return (
+                <li key={_key} className="sidebar-categories-item">
+                  <Link className="sidebar-categories-item-link" to={category}>
+                    {i18next.t(_key)}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </section>
+      <section className="section">
+        <Title color="skyblue" label="labels.sidebar.tags" />
+        <TagCloud language={language} tags={tags} />
+      </section>
+      <section className="section">
+        <ArchiveByMonth language={language} items={archiveByMonth} />
+      </section>
+    </div>
+  )
 }
+
+export default Sidebar
