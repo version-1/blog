52ab317dd79a9ee947d4a091525be0c8f5759a01
Jiro <j.code.public@gmail.com>
2021-03-20 01:31:31 -0700

    style sidebar

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -1,7 +1,9 @@
-import { IoSearch } from 'react-icons/io5'
+import { IoSearch, IoLogoGithub, IoLogoTwitter } from 'react-icons/io5'
 
 const icons = Object.freeze({
-  search: IoSearch
+  search: IoSearch,
+  github: IoLogoGithub,
+  twitter: IoLogoTwitter
 })
 
 const Icon = ({ icon, ...rest }: any) => {
--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,3 +1,13 @@
 type Lang = 'ja' | 'en'
 
 type Environment = 'production' | 'development'
+
+interface LayoutContext {
+  archiveByMonth: any
+  tags: string[]
+}
+
+interface PageContext {
+  layout: LayoutContext
+
+}
