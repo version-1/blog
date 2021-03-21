dd9af24fb0aa922680fb7be808d22b0847ac0690
version-1 <j.onodera26@gmail.com>
2021-03-21 14:33:14 -0700

    fix bottom post list

--- src/components/organisms/BottomPostList.tsx
+++ src/components/organisms/BottomPostList.tsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import Styles from 'lib/styles'
 import { instance as i18next } from 'lib/i18next'
-import HorizontalPostList from 'components/organisms/HorizontalPostList'
+import PostList from 'components/organisms/PostList'
+
+const styles = new Styles({
+  container: `
+    h2 {
+      text-align: center;
+      margin-bottom: 32px;
+    }
+
+    .row {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  `,
+}).style
 
 const BottomPostList = (props: any) => {
   const { posts, label, title } = props
@@ -10,9 +26,9 @@ const BottomPostList = (props: any) => {
 
   const _title = title || i18next.t(label)
   return (
-    <div className="bottom-posts">
+    <div css={styles.container} className="bottom-posts">
       <h2 className="bottom-post-title">{_title}</h2>
-      <HorizontalPostList posts={posts} />
+      <PostList posts={posts} />
     </div>
   )
 }
