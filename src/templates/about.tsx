fd2cd1344f7c1d9ae968266f79789516acf74160
Jiro <j.code.public@gmail.com>
2021-03-21 22:32:00 -0700

    style about page

--- src/templates/about.tsx
+++ src/templates/about.tsx
@@ -1,42 +1,162 @@
 import React, { useMemo } from 'react'
 import { graphql } from 'gatsby'
-import { meta } from 'config/constants'
 import Layout from 'components/layouts/Default'
-import Content, { HTMLContent } from 'components/Content'
+import { HTMLContent } from 'components/Content'
+import Styles from 'lib/styles'
+import { instance as i18next } from 'lib/i18next'
+import { colors } from 'constants/index'
+import Bar from 'components/atoms/Bar'
+import SNSButtons from 'components/organisms/SNSButtons'
+import Profile from 'components/organisms/Profile'
+import SearchCard from 'components/organisms/SearchCard'
+import ArticleIndex from 'components/organisms/ArticleIndex'
+import Header from 'components/organisms/ArticleHeader'
+import Promotion from 'components/organisms/Promotion'
 
-const AboutTemplate = ({ post, contentComponent }) => {
+const styles = new Styles({
+  container: `
+    max-width: 672px;
+    padding: 32px;
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0.8) 0%, rgba(255, 255, 255, 0.56) 100%);
+  `,
+  content: `
+    background: white;
+    border-radius: 8px;
+    padding: 32px 32px;
+    margin-bottom: 64px;
+  `,
+  postFooter: `
+    margin-bottom: 32px;
+  `,
+  share: `
+    flex: 1;
+    display: flex;
+    algin-items: center;
+    padding: 8px;
+    padding-left: 0;
+
+    p {
+      display: flex;
+    }
+
+    li {
+      margin-right: 4px;
+    }
+  `,
+  shareLabel: `
+    margin: auto 0;
+    margin-right: 16px;
+  `,
+  profile: `
+    flex: 1;
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    padding: 8px;
+    padding-right: 0;
+    margin-bottom: 16px;
+
+    h4 {
+      font-size: 14px;
+      font-weight: bold;
+    }
+
+    p {
+      width: 300px;
+    }
+
+    .header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .left {
+      flex: 1;
+    }
+
+    .right {
+      flex: 2;
+    }
+
+    .left,
+    .right {
+      padding: 4px;
+    }
+  `,
+  paging: `
+    display: flex;
+    justify-content: space-between;
+    font-size: 12px;
+
+    .left,
+    .right {
+      flex: 1;
+    }
+
+    .left {
+      padding-right: 8px;
+    }
+    .right {
+      padding-left: 8px;
+    }
+    .left > a,
+    .right > a {
+      color: ${colors.primaryColor};
+      font-weight: bold;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    a > p {
+      padding: 8px;
+    }
+  `,
+  promotion: `
+    width: 496px;
+    margin: auto;
+  `
+}).style
+
+const AboutTemplate = ({ location, post, meta, language }: any) => {
   const content = post.html
-  const { createdAt, updatedAt, title } = post.frontmatter
-  const PostContent = contentComponent || Content
+  const url = location.href
+
   return (
-    <section className="section">
-      <article className="post">
-        <h1 className="post-title">{title}</h1>
-        <div className="post-meta-header">
-          <div className="timestamp">
-            <div className="created-at">
-              <i className="tiny material-icons">create</i>
-              {createdAt}
-            </div>
-            <div className="updated-at">
-              <i className="tiny material-icons">autorenew</i>
-              {updatedAt}
+    <div css={styles.container}>
+      <section className="section">
+        <article css={styles.content} className="post">
+          <Header meta={meta} post={post} />
+          <Bar />
+          <HTMLContent className="post-body" content={content} />
+          <Bar />
+          <div css={styles.postFooter} className="post-meta-footer">
+            <div css={styles.share} className="sns-share-footer">
+              <p>
+                <label css={styles.shareLabel}>
+                  {i18next.t('labels.share')}
+                </label>
+                <SNSButtons url={url} size={28} />
+              </p>
             </div>
           </div>
+        </article>
+      </section>
+      <SearchCard />
+      <div css={styles.promotion}>
+        <Profile language={language} />
+      </div>
+      {language === 'ja' && (
+        <div css={styles.promotion}>
+          <Promotion language={language} />
         </div>
-        <PostContent className="post-body" content={content} />
-        <div className="post-meta-footer">
-          <div className="author">
-            Written By : <a href="#!">{meta.author}</a>
-          </div>
-        </div>
-        <div className="related-posts" />
-      </article>
-    </section>
+      )}
+    </div>
   )
 }
 
-const AboutPost = ({ data, pageContext, path }) => {
+const AboutPost = ({ location, data, pageContext, path }: any) => {
   const { markdownRemark: post } = data
   const context = useMemo(
     () => ({ ...pageContext, sidebarDisabled: true, path }),
@@ -44,8 +164,8 @@ const AboutPost = ({ data, pageContext, path }) => {
   )
 
   return (
-    <Layout context={context}>
-      <AboutTemplate post={post} contentComponent={HTMLContent} />
+    <Layout noconsole context={context} sidebar={<ArticleIndex {...post} />}>
+      <AboutTemplate {...context} post={post} location={location} />
     </Layout>
   )
 }
