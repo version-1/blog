0efbe951ec4376f428a45e14f52743868842fecf
Jiro <j.code.public@gmail.com>
2021-03-22 23:59:38 -0700

    refactor gatsby-node.js

--- gatsby-node/index.ts
+++ gatsby-node/index.ts
@@ -4,35 +4,28 @@ import { createRemoteFileNode, createFilePath } from 'gatsby-source-filesystem'
 import path from 'path'
 import config from '../config/constants'
 import { rootPath } from '../src/lib/routes'
-import { instance as i18next } from '../src/lib/i18next'
-import { validateCategoryList } from '../node/validation'
 import { fetch } from '../node/breadcrumbs'
 import { fetchPv } from '../node/pageview'
-import { rating } from '../node/related-post'
 import queries from '../node/queries'
 import { Lang, CreatePage } from '../gatsby-node/index.d'
+import {
+  PER_PAGE,
+  STATIC_PAGE_LIST,
+  genPath,
+  genSlugMap,
+  collectTags,
+  collectCategories
+} from '../gatsby-node/utils'
+import { createPostShowPage } from '../gatsby-node/posts/show'
+import { createPostsIndexPage } from '../gatsby-node/posts'
+import { createCategoryShowPage } from '../gatsby-node/categories/show'
+import { createTagShowPage } from '../gatsby-node/tags'
+import { createMonthArchivePage } from '../gatsby-node/months'
 
-const { routes, meta, constants } = config
+const { meta, constants } = config
 
 const isProduction = process.env.NODE_ENV === 'production'
 const dummyThumbnail = meta.images.url + '/others/dummy/thumbnail.png'
-
-// Constants
-const PER_PAGE = constants.per
-const STATIC_PAGE_LIST = constants.pages
-
-const genShowPath = (edge: any) => {
-  const { language, slug } = edge.frontmatter
-  const _path = slug || edge.fields.slug
-  return genPath(language, _path)
-}
-
-const genPath = (language: Lang, slug: string) => {
-  if (language && language !== 'ja') {
-    return ['/', language, '/', slug].join('').replace(/\/\//g, '/')
-  }
-  return slug
-}
 
 const create = (createPage: CreatePage) => (params: any) => {
   const { context = {} } = params
@@ -41,234 +34,6 @@ const create = (createPage: CreatePage) => (params: any) => {
     context: {
       ...context
     }
-  })
-}
-
-const buildPaginationPages = (createPage: any) => (
-  limit: number = PER_PAGE
-) => (
-  namespace: string,
-  templates: any,
-  totalCounts: number,
-  context: any = {}
-) => {
-  const totalPages = Math.ceil(totalCounts / limit)
-  Array.from({ length: totalPages }).forEach(
-    (_dummy: any, currentPageIndex: number) => {
-      const _path =
-        currentPageIndex === 0
-          ? namespace
-          : [namespace, currentPageIndex + 1].join('/')
-      const titleSuffix = currentPageIndex
-        ? `${currentPageIndex} / ${totalPages}`
-        : ''
-      const skip = currentPageIndex * limit
-      const url = [meta.siteUrl, _path].join('')
-      createPage({
-        path: _path,
-        component: path.resolve(`src/templates/${templates}.tsx`),
-        context: {
-          ...context,
-          limit,
-          skip,
-          index: currentPageIndex + 1,
-          totalPages,
-          meta: {
-            ...context.meta,
-            title: context.meta.title
-              ? `${context.meta.title} ${titleSuffix}`
-              : '',
-            url
-          }
-        }
-      })
-    }
-  )
-}
-
-const genAlternate = (paths: any[], orgLanguage: Lang, slug: string) => {
-  return Object.keys(paths).reduce((acc: any, key: any) => {
-    if (orgLanguage === key) {
-      return acc
-    }
-    if (!paths[key as any][slug]) {
-      return acc
-    }
-
-    return { ...acc, [key]: genPath(key, slug) }
-  }, {})
-}
-
-const genSlugMap = (posts: any) =>
-  posts.reduce(
-    (acc: any, item: any) => {
-      const {
-        frontmatter: { slug, language }
-      } = item
-      return {
-        ...acc,
-        [language]: { ...acc[language], [slug]: true }
-      }
-    },
-    { ja: {}, en: {} }
-  )
-
-interface Edges {
-  next: any
-  node: any
-  previous: any
-}
-
-const createPostShowPage = (createPage: CreatePage) => (
-  posts: any[],
-  edges: Edges[],
-  pageviews: any[],
-  slugMap: any[]
-) => (context: any) => {
-  edges.forEach(({ next, node: edge, previous }: Edges) => {
-    const { id, excerpt } = edge
-    const {
-      title,
-      canonical,
-      tags,
-      categories,
-      slug,
-      thumbnail,
-      templateKey
-    } = edge.frontmatter
-    const breadcrumbs = fetch(context.language)
-    const _breadcrumbs = [
-      ...context.layout.breadcrumbs,
-      breadcrumbs.categories(categories[0], context.language)
-    ]
-    validateCategoryList(edge, categories)
-    validateCategoryList(edge, tags)
-    const relatedRatings = rating(posts, edge, pageviews)
-
-    const _path = genShowPath(edge)
-    const baseUrl = [meta.siteUrl, slug].join('')
-    const url = [meta.siteUrl, _path].join('')
-    const alternate = genAlternate(slugMap, context.language, slug)
-    const image = [meta.images.url, thumbnail].join('')
-
-    createPage({
-      path: _path,
-      categories,
-      component: path.resolve(`src/templates/${String(templateKey)}.tsx`),
-      context: {
-        id,
-        ...context,
-        related: relatedRatings
-          .slice(0, 6)
-          .map((r: { slug: string }) => r.slug),
-        meta: {
-          title,
-          description: excerpt,
-          url,
-          canonical,
-          alternate,
-          image
-        },
-        layout: {
-          ...context.layout,
-          next,
-          previous,
-          baseUrl,
-          breadcrumbs: _breadcrumbs
-        }
-      }
-    })
-  })
-}
-
-const createPostsIndexPage = (createPage: CreatePage) => (
-  totalCount: number
-) => (context: any) => {
-  const _path = ['', routes.post].join('/')
-  buildPaginationPages(createPage)()(
-    genPath(context.language, _path),
-    'posts/index',
-    totalCount,
-    { ...context, meta: {} }
-  )
-}
-
-const createCollectionShowPage = (createPage: CreatePage) => (
-  key: string,
-  singuralizeKey: string,
-  collection: any
-) => (totalCount: number) => (context: any) => {
-  const _path = [
-    '',
-    routes[singuralizeKey as keyof typeof routes],
-    _.kebabCase(collection)
-  ].join('/')
-  const breadcrumbs: any = fetch(context.language)
-  const _breadcrumbs = [
-    ...context.layout.breadcrumbs,
-    breadcrumbs[key](collection, context.language)
-  ]
-  const heading = i18next.t(`${key}.${collection}`)
-  const path = genPath(context.language, _path)
-  const url = [meta.siteUrl, path].join('')
-  buildPaginationPages(createPage)()(path, key, totalCount, {
-    [singuralizeKey]: collection,
-    ...context,
-    layout: {
-      ...context.layout,
-      breadcrumbs: _breadcrumbs
-    },
-    meta: {
-      title: heading,
-      url
-    },
-    heading
-  })
-}
-
-const createCategoryShowPage = (createPage: CreatePage) => (
-  category: string
-) => (totalCount: number) => (context: any) => {
-  return createCollectionShowPage(createPage)(
-    'categories',
-    'category',
-    category
-  )(totalCount)(context)
-}
-
-const createTagShowPage = (createPage: CreatePage) => (tag: string) => (
-  totalCount: number
-) => (context: any) => {
-  return createCollectionShowPage(createPage)('tags', 'tag', tag)(totalCount)(
-    context
-  )
-}
-
-const createMonthArchivePage = (createPage: CreatePage) => (archives: any) => (
-  context: any
-) => {
-  Object.keys(archives).forEach((key) => {
-    const _path = genPath(context.language, ['', key].join('/'))
-    const totalCount = archives[key].length
-    const breadcrumbs = fetch(context.language)
-    const _breadcrumbs = [
-      ...context.layout.breadcrumbs,
-      breadcrumbs.monthArchive(key, context.language)
-    ]
-    const url = [meta.siteUrl, _path].join('')
-    buildPaginationPages(createPage)()(_path, 'months', totalCount, {
-      month: key,
-      ids: archives[key],
-      ...context,
-      layout: {
-        ...context.layout,
-        breadcrumbs: _breadcrumbs
-      },
-      meta: {
-        title: key,
-        url
-      }
-    })
   })
 }
 
@@ -300,24 +65,6 @@ const createStaticPage = (createPage: CreatePage) => (page: any) => (
     }
   })
 }
-
-const collectCollection = (posts: any) => (key: string) => {
-  return _.uniq(
-    _.compact(
-      _.flatten(
-        posts.map((post: any) => {
-          if (_.get(post, `frontmatter.${key}`)) {
-            return post.frontmatter[key]
-          }
-        })
-      )
-    )
-  )
-}
-
-const collectTags = (posts: any[]) => collectCollection(posts)('tags')
-const collectCategories = (posts: any[]) =>
-  collectCollection(posts)('categories')
 
 /* CreatePages
  *
@@ -443,7 +190,6 @@ export const onCreateNode = async ({
 }: any) => {
   const { createNode, createNodeField } = actions
   // fmImagesToRelative(node) // convert image paths for gatsby images
-  //
   if (
     node.internal.type === 'MarkdownRemark' &&
     node.frontmatter.featuredImgUrl !== null
