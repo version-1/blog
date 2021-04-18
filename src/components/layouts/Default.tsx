7664e679e5ce507e37aed5a4c4731809703ec8c7
Jiro <j.code.public@gmail.com>
2021-04-18 14:19:49 -0700

    Fix style

--- src/components/layouts/Default.tsx
+++ src/components/layouts/Default.tsx
@@ -61,13 +61,6 @@ const global = css`
     margin: 0;
   }
 
-  code.language-text {
-    font-size: 12px;
-    color: #ff4545 !important;
-    background: #f5f5f5 !important;
-    padding: 2px 4px !important;
-    border: 1px solid lightgray;
-  }
 `
 
 const styles = new Styles({
